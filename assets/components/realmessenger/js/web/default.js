5129a457a33f3c6c60406609725f757fcffcafa2
touol <touols@yandex.ru>
2021-01-21 23:32:17 +0700

    исправление ошибок

--- assets/components/realmessenger/js/web/default.js
+++ assets/components/realmessenger/js/web/default.js
@@ -231,6 +231,8 @@
                         $chat = RealMessenger.sendData.$chat;
                         $chat.remove();
                         $('#realmessenger-messages').html('');
+                        $('#realmessenger-message-form-wrapper').hide();
+                        $('.realmessenger-chats-empty').show();
                     };
                     RealMessenger.send(RealMessenger.sendData.data, RealMessenger.Chat.callbacks.close, RealMessenger.Callbacks.Chat.close);
                 
